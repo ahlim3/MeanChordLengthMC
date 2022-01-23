f167eaf9d5a71032f1ad494716ba354a951e04ec
ahlim3 <78980758+ahlim3@users.noreply.github.com>
2022-01-23 02:20:59 -0500

    Initial Commit

---
+++ Shared/MeanChordLengthMCApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeanChordLengthMCApp.swift
+//  Shared
+//
+//  Created by anthony lim on 1/23/22.
+//
+
+import SwiftUI
+
+@main
+struct MeanChordLengthMCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
